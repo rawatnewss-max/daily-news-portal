7b288d6dce389df8da8c894b1ecb91b9dc6e4ff3
rawatnewss-max <rawatnewss@gmail.com>
2026-09-19 14:10:16 +0530

    Update PublicShell.tsx

--- components/PublicShell.tsx
+++ components/PublicShell.tsx
@@ -1,11 +1,12 @@
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
-
+import { BreakingTicker } from "@/components/BreakingTicker";
 export function PublicShell({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+<BreakingTicker />
+<main>{children}</main>
       <Footer />
     </>
   );
